1e401755172de0fbe06d606ed1fd963a177398f8
David Rueck <drueck@gmail.com>
2014-06-12 14:37:16 -0700

    Can edit and update todo items

---
+++ app/assets/javascripts/collections/todo_items.js.coffee
@@ -0,0 +1,6 @@
+class App.Collections.TodoItems extends Backbone.Collection
+  initialize: (data, options) ->
+    @todoList = options.todoList
+
+  url: => "/todo_lists/#{@todoList.id}/todo_items"
+  model: App.Models.TodoItem
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root "notes#index"
   resources :notes, only: [:index, :create, :update, :destroy]
+  resources :todo_list, only: [] do
+    resources :todo_items, only: [:create, :update]
+  end
   get '*any' => 'notes#index'
 end
